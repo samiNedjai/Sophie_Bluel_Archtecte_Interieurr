335fe5ecabde46160eed0c0b41662f87da0aca05
samiNedjai <sami.nedjai@outlook.fr>
2023-05-05 16:47:53 +0200

    récupéré les projets depuis l’AP

---
+++ FrontEnd/index.js
@@ -0,0 +1,36 @@
+// ********* RECUPERATION DES DONNEES API (récupéré les projets) ********* //
+
+ const imagesListes = document.querySelector(".imagesListes");
+ const divGallery = document.createElement("div");
+ divGallery.classList.add("gallery");
+ imagesListes.appendChild(divGallery);
+
+ fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((works) => {
+    works.forEach((item) => {
+      const image = createImageWithCaption(
+        item.imageUrl,
+        item.title,
+        item.category.name
+      );
+    console.log(works)
+
+      divGallery.appendChild(image)
+    });
+    works.forEach((item => console.log(item)))
+
+  //La fonction  createImageWithCaption() sert à créer un élément figure contenant une image
+    // et une légende 
+    function createImageWithCaption(src, alt, caption) {
+        const figure = document.createElement("figure");
+        figure.classList.add("gallery-item");
+        const image = document.createElement("img");
+        image.src = src;
+        image.alt = alt;
+        figure.append(image,alt);
+
+        return figure;
+      }
+})
+        
